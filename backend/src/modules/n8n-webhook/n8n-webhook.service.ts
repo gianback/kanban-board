b57a5cf0b913d1949b033621dc16a6cce1df2d76
Gian Franco <gian_franco_08@hotmail.com>
2025-10-26 06:09:36 -0500

    feat: add envs for urls

--- backend/src/modules/n8n-webhook/n8n-webhook.service.ts
+++ backend/src/modules/n8n-webhook/n8n-webhook.service.ts
@@ -1,12 +1,13 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateN8nWebhookDto } from './dto/create-n8n-webhook.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class N8nWebhookService {
-  async create(createN8nWebhookDto: CreateN8nWebhookDto) {
-    console.log(createN8nWebhookDto);
+  constructor(private readonly configService: ConfigService) {}
 
-    const WEBHOOK_URL = 'http://localhost:5678/webhook-test/kanban-export';
+  async create(createN8nWebhookDto: CreateN8nWebhookDto) {
+    const WEBHOOK_URL = this.configService.get<string>('DATABASE_N8N') || '';
 
     const response = await fetch(WEBHOOK_URL, {
       method: 'POST',
